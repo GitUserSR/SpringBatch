77bf33f7cb41baa496bf0ff3d389eb89c586883d
srapolu <rapolu.swathi@gmail.com>
2020-03-15 13:27:58 -0700

    added src

---
+++ src/test/java/com/sr/learn/spring/batch/LearnSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sr.learn.spring.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
